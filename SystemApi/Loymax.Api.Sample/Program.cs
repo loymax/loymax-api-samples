93b937a7a5e7a7db5cdb266d4de311eb5b754bc2
a.kourakolov <a.kourakolov@loymax.ru>
2020-02-06 14:24:02 +0700

    Использование fluentApi для импорта акций

--- SystemApi/Loymax.Api.Sample/Program.cs
+++ SystemApi/Loymax.Api.Sample/Program.cs
@@ -11,36 +11,24 @@ namespace Loymax.Api.Sample
         {
             var client = new Client()
             {
-                BaseUrl = "https://armv2-test.loymax.tech/api/"
+                BaseUrl = "https://dev.loymax.tech/systemapi/"
             };
 
+            // учётные данные запросите у специалистов Loymax
             client.Authorization("login-here", "password-here");
 
-            var offers = new OffersImportModel {Version = ProtocolVersion.Version20, Offers = new List<OfferDto>()};
 
-            var offer = new OfferDto
-            {
-                Rules = new OfferRules {Events = new List<OfferEventDto>()}, Title = "Sample", Priority = 10,
-                Partners = new OfferPartners { Partners = new List<PartnerDto>()},
-                Id = Guid.NewGuid().ToString("N"),
-                IsSum = false,
-                WorkingState = OfferWorkingState.Running,
-                ChangesState = OfferChangesState.Approved,
-                ApplyChangesDate = DateTime.Now,
-                ExpirationDate = DateTime.Now.AddDays(1)
-            };
+            var offers = OfferImportBuilder.Create("SampleTitle") // Установить название акции
+                .WithPartner("43168568-a7f8-4582-9d45-49857634251d") // указываем id партнёра
+                .WithState(OfferWorkingState.Running) // Акция будет сразу запущена
+                .WithChanges(DateTime.Now, OfferChangesState.Approved) // применяется с момента загрузки
+                .WithExpirationDate(DateTime.Now.AddDays(1)) // Истекает срок действия через 1 день
+                .AddChain<PurchaseCalculateEventDto>() // добавляем цепочку для обработки события расчёта скидки
+                .WithCardStateFilter(CardState.Activated) // только для активированных карт
+                .WithDirectDiscount(ActionDiscountType.Percent, 0.1) // 10% прямой скидки
+                .Build();
 
-            offer.Partners.Partners.Add(new PartnerDto { Id = "43168568-a7f8-4582-9d45-49857634251d" });
-
-            var offerEvent = new PurchaseCalculateEventDto {Chains = new List<OfferActionsChainDto>()};
-            var chain = new OfferActionsChainDto {Name = "Первая цепочка", Order = 0, Actions = new List<ActionDto>(), Filters = new List<FilterDto>()};
-            chain.Actions.Add(new DirectDiscountActionDto { Percent = 0.1, DiscountType =  ActionDiscountType.Percent});
-
-            offerEvent.Chains.Add(chain);
-            offer.Rules.Events.Add(offerEvent);
-
-            offers.Offers.Add(offer);
-            var result = client.OfferImportExport_PostOffersAsync(offers).Result;
+            var result = client.OfferImportExport_PostOffersAsync(offers).Result; // импортируем акцию в систему
             Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
         }
     }
