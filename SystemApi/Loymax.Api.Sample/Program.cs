4a8256dc6bc3ff9d671a15411d24282bc86ffda1
Javirgi <62003810+Javirgi@users.noreply.github.com>
2020-04-01 09:18:08 +0500

    Update Program.cs

--- SystemApi/Loymax.Api.Sample/Program.cs
+++ SystemApi/Loymax.Api.Sample/Program.cs
@@ -14,21 +14,21 @@ namespace Loymax.Api.Sample
                 BaseUrl = "https://dev.loymax.tech/systemapi/"
             };
 
-            // учётные данные запросите у специалистов Loymax
+            // ask Loymax specialists for credentials
             client.Authorization("login-here", "password-here");
 
 
-            var offers = OfferImportBuilder.Create("SampleTitle") // Установить название акции
-                .WithPartner("43168568-a7f8-4582-9d45-49857634251d") // указываем id партнёра
-                .WithState(OfferWorkingState.Running) // Акция будет сразу запущена
-                .WithChanges(DateTime.Now, OfferChangesState.Approved) // применяется с момента загрузки
-                .WithExpirationDate(DateTime.Now.AddDays(1)) // Истекает срок действия через 1 день
-                .AddChain<PurchaseCalculateEventDto>() // добавляем цепочку для обработки события расчёта скидки
-                .WithCardStateFilter(CardState.Activated) // только для активированных карт
-                .WithDirectDiscount(ActionDiscountType.Percent, 0.1) // 10% прямой скидки
+            var offers = OfferImportBuilder.Create("SampleTitle") // Set offer name
+                .WithPartner("43168568-a7f8-4582-9d45-49857634251d") // Indicate partner id
+                .WithState(OfferWorkingState.Running) // The offer will be started immediately
+                .WithChanges(DateTime.Now, OfferChangesState.Approved) // It has been applied from the moment of loading
+                .WithExpirationDate(DateTime.Now.AddDays(1)) // Expires in 1 day
+                .AddChain<PurchaseCalculateEventDto>() // The chain for discount calculation event processing is added
+                .WithCardStateFilter(CardState.Activated) // for activated cards only
+                .WithDirectDiscount(ActionDiscountType.Percent, 0.1) // 10% direct discount
                 .Build();
 
-            var result = client.OfferImportExport_PostOffersAsync(offers).Result; // импортируем акцию в систему
+            var result = client.OfferImportExport_PostOffersAsync(offers).Result; // import the offer into the system
             Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
         }
     }
