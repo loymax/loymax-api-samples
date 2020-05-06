246a967af98d1cebb21ef8548634173ab74f6f60
Javirgi <62003810+Javirgi@users.noreply.github.com>
2020-05-06 15:52:56 +0500

    Update GoodsSetDiscountSample.cs
    Translation

--- SystemApi/Loymax.Api.Sample/GoodsSetDiscountSample.cs
+++ SystemApi/Loymax.Api.Sample/GoodsSetDiscountSample.cs
@@ -11,42 +11,42 @@ namespace Loymax.Api.Sample
     {
         public static async Task ImportOfferAsync(Client client, string partnerId)
         {
-            var goodsGroup = await client.GoodsGroup_GetGoodsGroupsAsync(filter_name: "Товары"); // Находим группу товаров с именем "Товары", которая должна быть предварительно создана в разделе "Группы товаров"
+            var goodsGroup = await client.GoodsGroup_GetGoodsGroupsAsync(filter_name: "Товары"); // Find a product group with the Products name, which must be previously created in the Product groups section
 
-            var offer = OfferImportBuilder.Create("Sapmle4. N+M") // Установливаем название акции
-                .WithPartner(partnerId) // Указываем id партнёра
-                .WithDescription("При покупке 5ти товаров, 6ой в подарок.") // Добавляем описание для акции
-                .WithState(OfferWorkingState.Running) // Акция будет сразу запущена
-                .WithChanges(DateTime.Now, OfferChangesState.Approved) // Применяется с установленной даты
-                .WithPriority(35) // Устанавливаем приоритет акции
-                .AddChain<PurchaseCalculateEventDto>() // Добавляем цепочку для обработки события расчёта скидки
-                .WithGoodsSetDivide( // Устанавливаем действие " Разбиение на наборы"
+            var offer = OfferImportBuilder.Create("Sapmle4. N+M") // Set the name of the offer
+                .WithPartner(partnerId) // Specify Partner's ID
+                .WithDescription("При покупке 5ти товаров, 6ой в подарок.") // Add a description for the offer
+                .WithState(OfferWorkingState.Running) // The offer will be run immediately
+                .WithChanges(DateTime.Now, OfferChangesState.Approved) // Applied from specified date
+                .WithPriority(35) // Set the priority of the offer
+                .AddChain<PurchaseCalculateEventDto>() // Add a chain to process discount calculation event
+                .WithGoodsSetDivide( // Set the Dividing into Sets action
                     new List<GoodsSetItemDto>
                     {
-                            new GoodsSetItemDto // Заводим новый набор
+                            new GoodsSetItemDto // Create a new set
                             {
-                                GoodsGroup = new GoodsGroupExDto // Источник товаров для набора является найденная группа товаров
+                                GoodsGroup = new GoodsGroupExDto // The item source for the set is the product group found.
                                 {
-                                    Id = goodsGroup.Data.First().ExternalId.Value.ToString(), // Устанавливаем Id группы товаров 
+                                    Id = goodsGroup.Data.First().ExternalId.Value.ToString(), // Set the product group ID 
                                 },
-                                Value = 6.000, // Задаем колиество товаров в наборе
+                                Value = 6.000, // Set the number of products in the set
                             },
                     },
-                    ChequeGoodsSortType.PriceAscending, // Устанавливаем тип сортировки товаров: По возрастанию цены
-                    ChequeGoodsSetType.Quantity) // Устанавливаем условие разбиения: Количество товара
-                .WithChequePositionGoodsFilter( // Устанавливаем фильтр "Товар"
-                    type: ChequeGoodsFilterType.GoodsQuantity, // Устанавливаем дополнительное условие, при котором будет срабатывать фильтр: Количество
-                    goodsGroupsIds: new List<Guid> { goodsGroup.Data.First().ExternalId.Value }, // Задаем найденную ранее группу товаров
-                    firstValue: 1.0, // Фильтр сработает только в том случае, если количество единиц товара в чеке меньше или равно 1.0
+                    ChequeGoodsSortType.PriceAscending, // Set the type of product sorting: Ascending prices
+                    ChequeGoodsSetType.Quantity) // Set the condition for dividing: Quantity of products
+                .WithChequePositionGoodsFilter( // Set the Product filter
+                    type: ChequeGoodsFilterType.GoodsQuantity, // Set an additional condition under which the filter will trigger: Quantity
+                    goodsGroupsIds: new List<Guid> { goodsGroup.Data.First().ExternalId.Value }, // Set the product group found earlier
+                    firstValue: 1.0, // The filter will only trigger if the number of units in the cheque is less than or equal to 1.0
                     @operator: ComparisonOperator.LessOrEqual,
                     chequeGoodsSortType: ChequeGoodsSortType.PriceAscending,
                     dividePositions: true)
-                .WithDirectDiscount( // Устанавливаем действие "Прямая скидка"
-                    dicsountType: ActionDiscountType.Percent, // Задаем способ расчета: Процент
-                    value: 100) // Устанавливаем размер скидки
+                .WithDirectDiscount( // Set the Direct Discount action
+                    dicsountType: ActionDiscountType.Percent, // Set the calculation method: Percentage
+                    value: 100) // Set the discount amount
                 .Build();
 
-            var result = await client.OfferImportExport_PostOffersAsync(offer); // Импортируем акцию в систему
+            var result = await client.OfferImportExport_PostOffersAsync(offer); // Import the offer into the system
             Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
         }
     }
