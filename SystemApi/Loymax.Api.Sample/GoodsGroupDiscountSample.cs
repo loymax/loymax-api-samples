e5f5fddc779477301335fa8bd1719f3721510a2a
Javirgi <62003810+Javirgi@users.noreply.github.com>
2020-05-06 14:14:58 +0500

    Update GoodsGroupDiscountSample.cs
    Translation

--- SystemApi/Loymax.Api.Sample/GoodsGroupDiscountSample.cs
+++ SystemApi/Loymax.Api.Sample/GoodsGroupDiscountSample.cs
@@ -11,25 +11,25 @@ namespace Loymax.Api.Sample
     {
         public static async Task ImportOfferAsync(Client client, string partnerId)
         {
-            var goodsGroup = await client.GoodsGroup_GetGoodsGroupsAsync(filter_name: "Товары"); // Находим группу товаров с именем "Товары", которая должна быть предварительно создана в разделе "Группы товаров"
+            var goodsGroup = await client.GoodsGroup_GetGoodsGroupsAsync(filter_name: "Товары"); // Find a product group with the Products name, which must be previously created in the Product groups section
 
-            var offer = OfferImportBuilder.Create("Sample1. Скидка на группу товаров.") // Установливаем название акции
-                .WithDescription("Скидка 10% на группу товаров с исключающим набором.") // Добавляем описание для акции
-                .WithPartner(partnerId) // Указываем id партнёра
-                .WithState(OfferWorkingState.Running) // Акция будет сразу запущена
-                .WithChanges(DateTime.Now, OfferChangesState.Approved) // Применяется с установленной даты
-                .WithPriority(20) // Устанавливаем приоритет акции
-                .AddChain<PurchaseCalculateEventDto>() // Добавляем цепочку для обработки события расчёта скидки
-                .WithChequePositionGoodsFilter( // Устанавливаем фильтр "Товар"
-                    type: ChequeGoodsFilterType.OnlyGoods, // Устанавливаем дополнительное условие, при котором будет срабатывать фильтр: Отсутствует
-                    goodsGroupsIds: new List<Guid> { goodsGroup.Data.First().ExternalId.Value }) // Задаем найденную ранее группу товаров
-                .WithDirectDiscount( // Устанавливаем действие "Прямая скидка"
-                    dicsountType: ActionDiscountType.Percent, // Задаем способ расчета: Процент
-                    value: 10.0, // Устанавливаем размер скидки
-                    calculationExclusionTypes: new List<CalculationExclusionDiscountType> { CalculationExclusionDiscountType.Discount }) // Исключаем из расчета сумму: Прямая скидка
+            var offer = OfferImportBuilder.Create("Sample1. Скидка на группу товаров.") // Set the name of the offer
+                .WithDescription("Скидка 10% на группу товаров с исключающим набором.") // Add a description for the offer
+                .WithPartner(partnerId) // Specify Partner's ID
+                .WithState(OfferWorkingState.Running) // The offer will be run immediately
+                .WithChanges(DateTime.Now, OfferChangesState.Approved) // Applied from specified date
+                .WithPriority(20) // Set the priority for the offer
+                .AddChain<PurchaseCalculateEventDto>() // Add a chain to process discount calculation event
+                .WithChequePositionGoodsFilter( // Set the Product filter
+                    type: ChequeGoodsFilterType.OnlyGoods, // Set an additional condition under which the filter will trigger: None
+                    goodsGroupsIds: new List<Guid> { goodsGroup.Data.First().ExternalId.Value }) // Set the product group found earlier
+                .WithDirectDiscount( // Set the Direct discount action
+                    dicsountType: ActionDiscountType.Percent, // Set the calculation method: Percentage
+                    value: 10.0, // Set the discount amount
+                    calculationExclusionTypes: new List<CalculationExclusionDiscountType> { CalculationExclusionDiscountType.Discount }) // Exclude from the calculation the amount: Direct discount
                 .Build();
 
-            var result = await client.OfferImportExport_PostOffersAsync(offer); // Импортируем акцию в систему
+            var result = await client.OfferImportExport_PostOffersAsync(offer); // Import the offer into the system
             Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
         }
     }
