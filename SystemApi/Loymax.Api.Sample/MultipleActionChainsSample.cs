3ac59b1a68b16edd6e956fab3b38e8797c23f14b
Javirgi <62003810+Javirgi@users.noreply.github.com>
2020-05-06 16:06:08 +0500

    Update MultipleActionChainsSample.cs
    Translation

--- SystemApi/Loymax.Api.Sample/MultipleActionChainsSample.cs
+++ SystemApi/Loymax.Api.Sample/MultipleActionChainsSample.cs
@@ -11,49 +11,49 @@ namespace Loymax.Api.Sample
     {
         public static async Task ImportOfferAsync(Client client, string partnerId)
         {
-            var goodsGroup = await client.GoodsGroup_GetGoodsGroupsAsync(filter_name: "Товары"); // Находим группу товаров с именем "Товары", которая должна быть предварительно создана в разделе "Группы товаров"
+            var goodsGroup = await client.GoodsGroup_GetGoodsGroupsAsync(filter_name: "Товары"); // Find a product group with the Products name, which must be previously created in the Product groups section
 
-            var offerBuilder = OfferImportBuilder.Create("Sample3. Фиксированная цена на единицу товара.") // Установливаем название акции
-                .WithDescription("Несколько цепочек с заданным фиксированном значением.") // Добавляем описание для акции
-                .WithPartner(partnerId) // Указываем id партнёра
-                .WithState(OfferWorkingState.Running) // Акция будет сразу запущена
-                .WithChanges(DateTime.Now, OfferChangesState.Approved) // Применяется с установленной даты
-                .WithPriority(30); // Устанавливаем приоритет акции
+            var offerBuilder = OfferImportBuilder.Create("Sample3. Фиксированная цена на единицу товара.") // Set the name of the offer
+                .WithDescription("Несколько цепочек с заданным фиксированном значением.") // Add a description for the offer
+                .WithPartner(partnerId) // Specify Partner's ID
+                .WithState(OfferWorkingState.Running) // The offer will be run immediately
+                .WithChanges(DateTime.Now, OfferChangesState.Approved) // Applied from specified date
+                .WithPriority(30); // Set the priority for the offer
 
-            // Добавляем 1-ю цепочку для обработки события расчёта скидки
+            // Add the 1st chain to process the discount calculation event.
             offerBuilder.AddChain<PurchaseCalculateEventDto>()
-                .WithChequePositionGoodsFilter( // Устанавливаем фильтр "Товар"
-                    type: ChequeGoodsFilterType.OnlyGoods, // Устанавливаем дополнительное условие, при котором будет срабатывать фильтр: Отсутствует
-                    goodsGroupsIds: new List<Guid> { goodsGroup.Data.First().ExternalId.Value }) // Задаем найденную ранее группу товаров
-                .WithDirectDiscount( // Устанавливаем действие "Прямая скидка"
-                    dicsountType: ActionDiscountType.PricePerUnit, // Задаем способ расчета: Фиксированная цена на единицу товара
-                    value: 450.0); // Устанавливаем размер скидки
+                .WithChequePositionGoodsFilter( // Set the Product filter
+                    type: ChequeGoodsFilterType.OnlyGoods, // Set an additional condition under which the filter will trigger: None
+                    goodsGroupsIds: new List<Guid> { goodsGroup.Data.First().ExternalId.Value }) // Set the product group found earlier
+                .WithDirectDiscount( // Set the Direct Discount action
+                    dicsountType: ActionDiscountType.PricePerUnit, // Set the calculation method: Fixed unit price
+                    value: 450.0); // Set the discount amount
 
-            // Добавляем 2-ю цепочку для обработки события расчёта скидки
+            // Add the 2nd chain to process the discount calculation event.
             offerBuilder.AddChain<PurchaseCalculateEventDto>()
-                .WithChequePositionGoodsFilter( // Устанавливаем фильтр "Товар"
-                    type: ChequeGoodsFilterType.OnlyGoods, // Устанавливаем дополнительное условие, при котором будет срабатывать фильтр: Отсутствует
+                .WithChequePositionGoodsFilter( // Set the Product filter
+                    type: ChequeGoodsFilterType.OnlyGoods, // Set an additional condition under which the filter will trigger: None
                     goodsGroupsIds: new List<Guid>
-                        {goodsGroup.Data.First().ExternalId.Value}) // Задаем найденную ранее группу товаров
-                .WithDirectDiscount( // Устанавливаем действие "Прямая скидка"
+                        {goodsGroup.Data.First().ExternalId.Value}) // Set the product group found earlier
+                .WithDirectDiscount( // Set the Direct Discount action
                     dicsountType: ActionDiscountType
-                        .PricePerUnit, // Задаем способ расчета: Фиксированная цена на единицу товара
-                    value: 350.0); // Устанавливаем размер скидки
+                        .PricePerUnit, // Set the calculation method: Fixed unit price
+                    value: 350.0); // Set the discount amount
 
-            // Добавляем 3-ю цепочку для обработки события расчёта скидки
+            // Add the 3rd chain to process the discount calculation event.
             offerBuilder.AddChain<PurchaseCalculateEventDto>()
-                .WithChequePositionGoodsFilter( // Устанавливаем фильтр "Товар"
-                    type: ChequeGoodsFilterType.OnlyGoods, // Устанавливаем дополнительное условие, при котором будет срабатывать фильтр: Отсутствует
+                .WithChequePositionGoodsFilter( // Set the Product filter
+                    type: ChequeGoodsFilterType.OnlyGoods, // Set an additional condition under which the filter will trigger: None
                     goodsGroupsIds: new List<Guid>
-                        {goodsGroup.Data.First().ExternalId.Value}) // Задаем найденную ранее группу товаров
-                .WithDirectDiscount( // Устанавливаем действие "Прямая скидка"
+                        {goodsGroup.Data.First().ExternalId.Value}) // Set the product group found earlier
+                .WithDirectDiscount( // Set the Direct Discount action
                     dicsountType: ActionDiscountType
-                        .PricePerUnit, // Задаем способ расчета: Фиксированная цена на единицу товара
-                    value: 250.0); // Устанавливаем размер скидки
+                        .PricePerUnit, // Set the calculation method: Fixed unit price
+                    value: 250.0); // Set the discount amount
 
             var offer = offerBuilder.Build();
 
-            var result = await client.OfferImportExport_PostOffersAsync(offer); // Импортируем акцию в систему
+            var result = await client.OfferImportExport_PostOffersAsync(offer); // Import the offer into the system
             Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
         }
     }
